9b30c47ad4e891f66a752dc4c8fabc8edfb94bb2
Adityaraj Singh <rajsinghaditya512@gmail.com>
2024-08-14 18:56:27 +0530

    added color button feature

---
+++ bodycolor/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+function App() {
+  const [color, setColor] = useState("gray")
+  return (
+    <div className='w-full h-screen duration-200'
+    style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'>
+          <button
+          onClick={()=> setColor('red')} 
+          className='outline-black px-4 py-1 rounded-full text-white shadow-sm'
+          style={{backgroundColor:"red"}}>Red</button>
+          <button 
+          onClick={()=> setColor('green')} 
+          className='outline-black px-4 py-1 rounded-full text-white shadow-sm'
+          style={{backgroundColor:"Green"}}>Green</button>
+          <button 
+          onClick={()=> setColor('black')} 
+          className='outline-black px-4 py-1 rounded-full text-white shadow-sm'
+          style={{backgroundColor:"Black"}}>Black</button>
+          <button 
+          onClick={()=> setColor('pink')} 
+          className='outline-black px-4 py-1 rounded-full text-white shadow-sm'
+          style={{backgroundColor:"Pink"}}>Pink</button>
+          <button 
+          onClick={()=> setColor('orange')} 
+          className='outline-black px-4 py-1 rounded-full text-white shadow-sm'
+          style={{backgroundColor:"Orange"}}>Orange</button>
+          <button 
+          onClick={()=> setColor('Blue')} 
+          className='outline-black px-4 py-1 rounded-full text-white shadow-sm'
+          style={{backgroundColor:"Blue"}}>Blue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
