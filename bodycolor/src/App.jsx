b7ae6cd9c68405254e058d4e5b8bc64410321b0e
Adityaraj Singh <rajsinghaditya512@gmail.com>
2024-08-14 21:02:56 +0530

    Update App.jsx

--- bodycolor/src/App.jsx
+++ bodycolor/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 
 function App() {
   const [color, setColor] = useState("gray")
-  return (
+  return (<>
+  <h1 className="text-white align-middle text-center py-80"    
+          style={{backgroundColor:color}}>Now the color is: {color}</h1>
     <div className='w-full h-screen duration-200'
     style={{backgroundColor:color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
@@ -31,9 +33,12 @@ function App() {
           onClick={()=> setColor('Blue')} 
           className='outline-black px-4 py-1 rounded-full text-white shadow-sm'
           style={{backgroundColor:"Blue"}}>Blue</button>
+          <h1 className="text-white rounded-xl  px-3 py-2"    
+          style={{backgroundColor:color}}>Now the color is: {color}</h1>
         </div>
       </div>
     </div>
+    </>
   )
 }
 
