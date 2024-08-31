d77ce4bc2d737c5d4a21a14d6471dc55d5d93c25
Adityaraj Singh <rajsinghaditya512@gmail.com>
2024-08-31 19:27:54 +0530

    .

--- contextProvider/src/App.jsx
+++ contextProvider/src/App.jsx
@@ -1,10 +1,13 @@
+import Login from "./components/Login"
+import Profile from "./components/Profile"
 import UserContextProvider from "./context/ContextProvider"
 
 function App() {
 
   return (
    <UserContextProvider>
-    
+    <Login/>
+    <Profile/>
    </UserContextProvider>
   )
 }
--- contextProvider/src/components/Login.jsx
+++ contextProvider/src/components/Login.jsx
@@ -7,8 +7,9 @@ const [password, setPassword] = useState('')
 
 const {setUser} = useContext(UserContext)
 
-  const handleSubmit = () =>{
-
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    setUser({username, password})
   }
   return (
     <div>
@@ -17,10 +18,11 @@ const {setUser} = useContext(UserContext)
       value={username}
       onChange={(e)=> setUsename(e.target.value)}
       placeholder='usename'/>
+      {"  "}
       <input type='text' 
       value={password}
       onChange={(e)=>setPassword(e.target.value)}
-      placeholder='usename'/>
+      placeholder='password'/>
       <button onClick={handleSubmit}>Submit</button>
     </div>
   )
